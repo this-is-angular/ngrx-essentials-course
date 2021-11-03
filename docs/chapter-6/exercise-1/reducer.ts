7344a6f137088b6b477d66b77eefdf8df919b536
Alain Boudard <coco.boudard@gmail.com>
2021-11-03 16:39:22 +0100

    fix: action props

--- docs/chapter-6/exercise-1/reducer.ts
+++ docs/chapter-6/exercise-1/reducer.ts
@@ -1,10 +1,10 @@
 const _reducer = createReducer(
   initialState,
   // other handlers
-  on(deleteCategory, (state, action) => ({
+  on(actions.deleteCategory, (state, category) => ({
     ...state,
     categories: state.categories.filter(
-      (category) => category.name !== action.payload
+      (cat) => cat.name !== category.name
     ), // filter out the deleted category
   }))
 );
